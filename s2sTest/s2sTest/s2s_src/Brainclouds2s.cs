45999300cf89d7145ac9d37d7a3a0fdac8d42e2b
unknown <ryguyruth3@hotmail.com>
2020-04-14 21:03:58 -0400

    additional cleanup

--- s2sTest/s2sTest/s2s_src/Brainclouds2s.cs
+++ s2sTest/s2sTest/s2s_src/Brainclouds2s.cs
@@ -18,13 +18,7 @@ using System.Collections;
 using System.Runtime.Serialization;
 using BrainCloud.JsonFx.Json;
 
-//public interface IS2SCallback
-//{
-//    void onAuthenticationCallback(Dictionary<string, object> responseData);
-//    void onHeartbeatCallback(Dictionary<string, object> responseData);
-//}
-
-internal sealed class BrainCloudS2S //: IS2SCallback
+internal sealed class BrainCloudS2S
 {
     private static int NO_PACKET_EXPECTED = -1;
     private static int SERVER_SESSION_EXPIRED = 40365;
